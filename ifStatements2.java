16514cfbcce94b8623af63fed09265bf8877b3dd
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2024-09-30 12:07:53 -0400

    Create ifStatements2.java

---
+++ ifStatements2.java
@@ -0,0 +1,12 @@
+public class ifStatements2 {
+  public static void main(String[] args) {
+    System.out.println("Problem 6");
+    int jersynumber = 90;
+    if (jersynumber == 12 || jersynumber == 71 || jersynumber == 80){
+      System.out.println("That number is retired from the Seattle Seahawks!");
+    }
+    else{
+      System.out.println("That number is not retired from the Seattle Seahawks!");
+    } 
+  }
+}
